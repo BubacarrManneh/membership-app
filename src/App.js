6a61be165ad9f6852e8a8abf20d7bf5e04d138d2
BubacarrManneh <bubacarr.manneh@integrify.io>
2023-07-29 15:08:00 +0200

    Setting a background image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import Home from './Pages/Home';
 import './Styles/styles.css';
 
+import background from "./bacgrounds/bg-2.jpg"
+
 import {Route, Routes } from 'react-router-dom';
 import SignInPage from './Pages/SignInPage'
 import RegistrationPage from './Pages/RegistrationPage';
@@ -9,7 +11,7 @@ import RegistrationPage from './Pages/RegistrationPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", alignContent: "center"}}>
       <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignInPage />} />
