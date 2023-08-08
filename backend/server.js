9777b91a96d4c5bffa0bb6a331bb890e7789afd4
Bubacarr .M. Manneh <88854411+BubacarrManneh@users.noreply.github.com>
2023-08-08 18:06:41 +0200

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const app = require('./app')
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on PORT : ${PORT}`)
-})
-
